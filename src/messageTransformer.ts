863ec716cda649fe601d62823d7a0f324b3b9762
Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
2024-05-23 18:30:53 -0300

    Update messageTransformer.ts

--- src/messageTransformer.ts
+++ src/messageTransformer.ts
@@ -19,6 +19,7 @@ export const messageTransformer = (): ts.TransformerFactory<ts.SourceFile> => {
   const transformerFactory: ts.TransformerFactory<ts.SourceFile> = (context) => (sourceFile) => {
     if (
       // if there are no messages, no transformation is needed
+      // dummy comment
       !sourceFile.statements.some((i) => ts.isImportDeclaration(i) && i.importClause?.getText().includes('Messages')) ||
       // don't transform the transformer itself
       sourceFile.fileName.includes('messageTransformer.ts')
